3aa8b2cd715ba0a226c31d8fcafd535dd0703a30
Bakare Damilola <bdamilola00@gmail.com>
2023-08-05 11:07:41 +0100

    removed text-color attribute on input box

--- GUI/gui.py
+++ GUI/gui.py
@@ -2,7 +2,7 @@ from CLI.todo import get_todo_file_path, add_todo, show_todo, edit_todo, complet
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo", text_color="blue")
+input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
 window = sg.Window("My To-Do App", layout=[[label], [input_box, add_button]])
 window.read()
